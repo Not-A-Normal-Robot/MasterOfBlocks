b42d622cb2248562f63a4abf30154bc3d6708895
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2022-11-11 20:04:38 +0700

    add mob4 old versions

--- MoB/MoB4/src/conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf(t)
-  t.window.title="Master of Blocks: Apotheosis"
-  t.window.icon="resources/sprites/joe.png"
-  t.window.width=640
-  t.window.height=480
-  t.window.resizable=true
-end
